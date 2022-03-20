bd5cb4e5a0313a64c35ab1bf73d8ed12856ab945
takaakisonobe <a.nex201@gmail.com>
2022-03-21 01:09:38 +0900

    メイン関数修正

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import pyFxUtil
 import time
 import numpy as np
 
+import sys
+
+args = sys.argv
+if len(args) != 3:
+    print("invalid arguments recieved.")
+    print("[" + str(args[0]) + "]")
+    print("usage : python main.py <mode->0:test 1:release><process_interval(sec)> ")
+
+#実行モード
+mode = int(args[1])
+
+#価格取得間隔
+interval = int(args[2])
+
 #api取得
 try:
     api = bitflyerApiUtil.getBemApi('config.ini')
@@ -17,9 +31,6 @@ except:
 
 #価格保持日数
 days = 30
-
-#価格取得間隔
-interval = 60
 
 #価格保持配列
 prices = np.zeros(days * 24 * 60)
@@ -34,4 +45,5 @@ while True:
     #価格保持配列更新
     prices = pyFxUtil.updatePrices(prices, price)
 
-    
+    if mode == 0:
+        print(prices)
